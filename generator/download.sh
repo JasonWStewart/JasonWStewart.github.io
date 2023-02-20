d4c2b7eba502bb7d98b0c6ed8bd310aa25fd5307
Jason Stewart <jason@jasonstewart.io>
2023-02-20 10:22:25 +0000

    fix download

--- generator/download.sh
+++ generator/download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 DATE=`date +'%Y-%m-%d'`
 mkdir ./data
-curl -o ./data/$DATE-fotmobData.json https://www.fotmob.com/api/leagues\?id\=8944
+curl -o ./data/$DATE-fotmobData.json 'https://www.fotmob.com/api/leagues?id=8944'
